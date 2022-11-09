a184deafe759852d4b43c67963e9812920ae4f40
Matteogatto94 <cosenza94@live.it>
2022-11-09 15:36:59 +0100

    final commit with correction

--- my-vue-app/src/components/AppTitle.vue
+++ my-vue-app/src/components/AppTitle.vue
@@ -1,3 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+
 <template>
     <h1>La mia prima app con Vite!</h1>
 </template>
+
+<style>
+h1 {
+    color: red;
+    background-color: yellow;
+}
+</style>
